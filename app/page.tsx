5ad6dff46d5a1d5afa9c3e2c717c2b21abb0c44f
Valentina Valverde <vvgreenville@gmail.com>
2024-03-06 17:58:38 -0500

    ***IMPORTANT***
    run this:
    npm install @heroicons/react
    
    Added AdminLogin component and used TailwindCSS code. You will get an error if you do not run the above script.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import LangingPage from "./components/LandingPage";
 import NavBar from "./components/Navbar";
+import AdminLogin from "./components/AdminLogin";
 
 export default function Home() {
   return (
     <main className="">
       <NavBar />
       <LangingPage />
+      <AdminLogin />
     </main>
   );
 }
