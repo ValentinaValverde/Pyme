3064fba0c416a403c42b295048fc5b2fc6fcebeb
Valentina Valverde <vvgreenville@gmail.com>
2024-03-06 16:38:36 -0500

    - Cleaned out everything in page.tsx - Created component folder in app - created Landing Page component - Imported Landing Page component to page.tsx - Created styling folder in app - Moved globals.css file to styling folder - Changed css import in layout.tsx - Added storeadminfacing.css and customerfacing.css files in styling folder

---
+++ app/components/LandingPage.tsx
@@ -0,0 +1,11 @@
+export default function LangingPage() {
+  return (
+    <>
+      <div>
+        <p>welcome to</p>
+        <h1>Pyme</h1>
+        <p>the marketplace for small businesses.</p>
+      </div>
+    </>
+  );
+}
