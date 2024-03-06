9f03928f76d170509ad2ed887167d4dcf698931b
Valentina Valverde <vvgreenville@gmail.com>
2024-03-06 17:04:42 -0500

    Created and styled Navbar component

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+export default function NavBar() {
+  return (
+    <>
+      <nav className="navbar">
+        <div>
+          <p>logo</p>
+        </div>
+        <div>
+          <p>etc</p>
+        </div>
+      </nav>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import LangingPage from "./components/LandingPage";
+import NavBar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="">
+      <NavBar />
       <LangingPage />
     </main>
   );
