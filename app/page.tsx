356bb4af87c8915054501e0ff7b05f584859b03c
Valentina Valverde <vvgreenville@gmail.com>
2024-03-10 15:03:51 -0400

    Cleared out components from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 import LangingPage from "./components/LandingPage";
 import NavBar from "./components/Navbar";
-import AdminLogin from "./components/AdminLogin";
-import AdminInfoCards from "./components/AdminInfoCards";
 
 export default function Home() {
   return (
     <main className="">
       <NavBar />
       <LangingPage />
-      {/* <AdminLogin /> */}
-      <AdminInfoCards />
     </main>
   );
 }
