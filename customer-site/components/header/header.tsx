5fef0a9666a8e8b512c43916de01a8ac68606267
jlotten <jenna.otten@gmail.com>
2024-03-10 12:01:25 -0700

    Added initial customer product page

---
+++ customer-site/components/header/header.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <div className="navbar justify-between bg-base-300 text-green-900">
+          <Link href="/" className="btn btn-ghost text-lg">
+            Pyme | [piˈme]
+          </Link>
+          <ul className="flex">
+            <li>
+              <Link className="btn btn-ghost rounded-btn" href="/cart">
+                Cart
+              </Link>
+            </li>
+            <li>
+              <Link className="btn btn-ghost rounded-btn" href="/cart">
+                Sign In
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
