948ec269dd6294f8f4391aa61fb0abc665b147f7
Valentina Valverde <vvgreenville@gmail.com>
2024-03-17 21:22:00 -0400

    Styled Edit Product Inv Form component

--- components/EditProductInvForm.tsx
+++ components/EditProductInvForm.tsx
@@ -1,25 +1,56 @@
-import React from 'react'
-import { updateInventory } from '@/utils/actions/productActions'
+import React from "react";
+import { updateInventory } from "@/utils/actions/productActions";
 
 const EditProductInvForm = ({ product, myStore }: any) => {
-	const { inInv, productSlug } = product
-	return (
-		<form action={updateInventory}>
-			<input
-				className='input input-bordered join-item w-full'
-				placeholder='inInv'
-				type='number'
-				defaultValue={inInv}
-				name='inInv'
-				required
-			/>
-			<input type='hidden' name='productSlug' value={productSlug} />
-			<input type='hidden' name='myStore' value={myStore} />
-			<button type='submit' className='btn join-item btn-primary'>
-				Update
-			</button>
-		</form>
-	)
-}
+  const { inInv, productSlug } = product;
+  return (
+    <>
+      <div className="styled_form">
+        <form action={updateInventory}>
+          <div className="space-y-12">
+            <div className="border-b border-gray-900/10 pb-12">
+              <h2 className="text-base font-semibold leading-7 text-gray-900">
+                Update Inventory
+              </h2>
 
-export default EditProductInvForm
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="sm:col-span-4">
+                  <label className="block text-sm font-medium leading-6 text-gray-900">
+                    Inventory
+                  </label>
+                  <input
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    type="number"
+                    name="inInv"
+                    defaultValue={inInv}
+                    required
+                  />
+                </div>
+
+                <input type="hidden" name="productSlug" value={productSlug} />
+                <input type="hidden" name="myStore" value={myStore} />
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-6 flex items-center justify-end gap-x-6">
+            <button
+              type="button"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="submit-button rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default EditProductInvForm;
