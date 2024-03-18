572544c879f7f59d245fd4cf96ced83bcef39c7a
Valentina Valverde <vvgreenville@gmail.com>
2024-03-17 21:18:03 -0400

    Styled edit products form

--- components/EditProductForm.tsx
+++ components/EditProductForm.tsx
@@ -1,60 +1,115 @@
-'use client'
-import React from 'react'
-import { editStoreProduct } from '@/utils/actions/productActions'
+"use client";
+import React from "react";
+import { editStoreProduct } from "@/utils/actions/productActions";
 
 const EditProductForm = ({ product, myStore }: any) => {
-	const { productName, inInv, productDetails, price, productSlug } = product
+  const { productName, inInv, productDetails, price, productSlug } = product;
 
-	return (
-		<form action={editStoreProduct}>
-			<div className='join w-full'>
-				<input
-					className='input input-bordered join-item w-full'
-					placeholder='productName'
-					type='text'
-					defaultValue={productName}
-					name='productName'
-					required
-				/>
-				<input
-					className='input input-bordered join-item w-full'
-					placeholder='productDetails'
-					type='text'
-					defaultValue={productDetails}
-					name='productDetails'
-					required
-				/>
-				<input
-					className='input input-bordered join-item w-full'
-					placeholder='inInv'
-					type='number'
-					defaultValue={inInv}
-					name='inInv'
-					required
-				/>
-				<input
-					className='input input-bordered join-item w-full'
-					placeholder='price'
-					type='text'
-					defaultValue={price}
-					name='price'
-					required
-				/>
-				<input
-					className='input input-bordered join-item w-full'
-					type='text'
-					placeholder='image url'
-					name='productImage'
-					required
-				/>
-				<input type='hidden' name='productSlug' value={productSlug} />
-				<input type='hidden' name='myStore' value={myStore} />
-				<button type='submit' className='btn join-item btn-primary'>
-					Submit
-				</button>
-			</div>
-		</form>
-	)
-}
+  return (
+    <>
+      <div className="styled_form">
+        <form action={editStoreProduct}>
+          <div className="space-y-12">
+            <div className="border-b border-gray-900/10 pb-12">
+              <h2 className="text-base font-semibold leading-7 text-gray-900">
+                Edit Product
+              </h2>
 
-export default EditProductForm
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="sm:col-span-4">
+                  <label className="block text-sm font-medium leading-6 text-gray-900">
+                    Product Name
+                  </label>
+                  <input
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    type="text"
+                    name="productName"
+                    defaultValue={productName}
+                    required
+                  />
+                </div>
+
+                <div className="col-span-full">
+                  <label className="block text-sm font-medium leading-6 text-gray-900">
+                    Product Details
+                  </label>
+                  <textarea
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    rows={3}
+                    name="productDetails"
+                    defaultValue={productDetails}
+                    required
+                  />
+                  <p className="mt-3 text-sm leading-6 text-gray-600">
+                    Describe your product
+                  </p>
+                </div>
+
+                <div className="sm:col-span-4">
+                  <label className="block text-sm font-medium leading-6 text-gray-900">
+                    inInv
+                  </label>
+                  <input
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    type="number"
+                    name="inInv"
+                    defaultValue={inInv}
+                    required
+                  />
+                </div>
+
+                <div className="sm:col-span-4">
+                  <label className="block text-sm font-medium leading-6 text-gray-900">
+                    Price
+                  </label>
+                  <input
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    type="number"
+                    name="price"
+                    defaultValue={price}
+                    required
+                  />
+                </div>
+
+                <div className="sm:col-span-4">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Product Image URL
+                  </label>
+                  <input
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    type="text"
+                    name="productImage"
+                    required
+                  />
+                </div>
+
+                <input type="hidden" name="productSlug" value={productSlug} />
+                <input type="hidden" name="myStore" value={myStore} />
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-6 flex items-center justify-end gap-x-6">
+            <button
+              type="button"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="submit-button rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default EditProductForm;
