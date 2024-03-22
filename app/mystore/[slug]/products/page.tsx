8be908d0cf4b99a3dde862e7aaaad6c2727a0c9a
jlotten <jenna.otten@gmail.com>
2024-03-22 13:46:34 -0700

    Connected Add product buttonto product details page

--- app/mystore/[slug]/products/page.tsx
+++ app/mystore/[slug]/products/page.tsx
@@ -3,25 +3,28 @@ import ProductList from '@/components/ProductList'
 import Sidebar from '@/components/Sidebar'
 import { Box } from '@mui/material'
 import { Button } from '@mui/material'
+import Link from 'next/link'
 
 const myProductsPage = ({ params }: any) => {
 	return (
 		<>
 			<Box sx={{ display: 'flex' }}>
 				<Sidebar storeSlug={params.slug} />
-				<Box component={'main'} sx={{ flexGrow: 1, p: 3 }}>
-					<Box display='flex' justifyContent='flex-end' mb={2}>
-						<Button
-							variant='contained'
-							style={{ backgroundColor: 'green', color: 'white' }}
-						>
-							+ Add Product
-						</Button>
-					</Box>
-					<Box sx={{ display: 'flex' }}>
-						<ProductList myStore={params.slug} />
-					</Box>
-				</Box>
+                <Box component={'main'} sx={{ flexGrow: 1, p: 3 }}>
+                  <Box display='flex' justifyContent='flex-end' mb={2}>
+                    <Link href={`/mystore/${params.slug}/products/create`}> 
+                    <Button
+                      variant='contained'
+                      style={{ backgroundColor: 'green', color: 'white' }}
+                    >
+                      + Add Product
+                    </Button>
+                    </Link>
+                  </Box>
+                  <Box sx={{ display: 'flex' }}>
+                    <ProductList myStore={params.slug} />
+                  </Box>
+                </Box>
 			</Box>
 		</>
 	)
