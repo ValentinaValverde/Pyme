b03d6fd7890ff1692ac1d5a17fcf98ce1e999444
Valentina Valverde <vvgreenville@gmail.com>
2024-03-22 12:10:39 -0400

    Styled StoreSignUpForm

--- components/SignupForm.tsx
+++ components/SignupForm.tsx
@@ -1,39 +1,41 @@
-'use client'
-import React from 'react'
-import { createUser } from '@/utils/actions/userActions'
+'use client';
+import React from 'react';
+import { createUser } from '@/utils/actions/userActions';
 
 const SignupForm = () => {
-	return (
-		<form action={createUser}>
-			<div className='join w-full'>
-				<input
-					className='input input-bordered join-item w-full'
-					placeholder='username'
-					type='text'
-					name='username'
-					required
-				/>
-				<input
-					className='input input-bordered join-item w-full'
-					placeholder='email'
-					type='email'
-					name='email'
-					required
-				/>
-				<input
-					className='input input-bordered join-item w-full'
-					placeholder='password'
-					type='password'
-					name='password'
-					required
-				/>
-				<input type='hidden' name='role' value={'Buisness'} />
-				<button type='submit' className='btn join-item btn-primary'>
-					Sign Up
-				</button>
-			</div>
-		</form>
-	)
-}
+  return (
+    <>
+      <form action={createUser}>
+        <div className="join w-full">
+          <input
+            className="input input-bordered join-item w-full"
+            placeholder="username"
+            type="text"
+            name="username"
+            required
+          />
+          <input
+            className="input input-bordered join-item w-full"
+            placeholder="email"
+            type="email"
+            name="email"
+            required
+          />
+          <input
+            className="input input-bordered join-item w-full"
+            placeholder="password"
+            type="password"
+            name="password"
+            required
+          />
+          <input type="hidden" name="role" value={'Buisness'} />
+          <button type="submit" className="btn join-item btn-primary">
+            Sign Up
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
 
-export default SignupForm
+export default SignupForm;
--- components/StoreSignUpForm.tsx
+++ components/StoreSignUpForm.tsx
@@ -1,46 +1,91 @@
-'use client'
+'use client';
 
-import React from 'react'
-import { createStore } from '@/utils/actions/storeActions'
+import React from 'react';
+import { createStore } from '@/utils/actions/storeActions';
 
 const StoreSignUpForm = () => {
-	return (
-		<form action={createStore}>
-			<div className='join w-full'>
-				<input
-					className='input input-bordered join-item w-full'
-					placeholder='storename'
-					type='text'
-					name='storename'
-					required
-				/>
-				<input
-					className='input input-bordered join-item w-full'
-					placeholder='ownername'
-					type='text'
-					name='ownername'
-					required
-				/>
-				<input
-					className='input input-bordered join-item w-full'
-					placeholder='mockuserid'
-					type='number'
-					name='mockuserid'
-					required
-				/>
-				<input
-					className='input input-bordered join-item w-full'
-					placeholder='ein'
-					type='number'
-					name='ein'
-					required
-				/>
-				<button type='submit' className='btn join-item btn-primary'>
-					Submit
-				</button>
-			</div>
-		</form>
-	)
-}
+  return (
+    <>
+      <div className="styled_form">
+        <form action={createStore}>
+          <div className="space-y-12">
+            <div className="border-b border-gray-900/10 pb-12">
+              <h2 className="text-base font-semibold leading-7 text-gray-900">
+                Sign Up!
+              </h2>
 
-export default StoreSignUpForm
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="sm:col-span-4">
+                  <label className="block text-sm font-medium leading-6 text-gray-900">
+                    Store Name
+                  </label>
+                  <input
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    type="text"
+                    name="storename"
+                    required
+                  />
+                </div>
+
+                <div className="sm:col-span-4">
+                  <label className="block text-sm font-medium leading-6 text-gray-900">
+                    Owner Name
+                  </label>
+                  <input
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    type="text"
+                    name="ownername"
+                    required
+                  />
+                </div>
+
+                <div className="sm:col-span-4">
+                  <label className="block text-sm font-medium leading-6 text-gray-900">
+                    Mock User ID
+                  </label>
+                  <input
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    type="number"
+                    name="mockuserid"
+                    required
+                  />
+                </div>
+
+                <div className="sm:col-span-4">
+                  <label className="block text-sm font-medium leading-6 text-gray-900">
+                    EIN
+                  </label>
+                  <input
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    type="number"
+                    name="ein"
+                    required
+                  />
+                </div>
+
+                <input type="hidden" name="role" value={'Buisness'} />
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-6 flex items-center justify-end gap-x-6">
+            {/* <button
+              type="button"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Cancel
+            </button> */}
+            <button
+              type="submit"
+              className="submit-button rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default StoreSignUpForm;
