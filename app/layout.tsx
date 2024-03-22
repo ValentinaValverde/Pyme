e2776ae7bc00c173378cf624c626ec62cdf2bedf
rugby8724 <jeremiah.d.wise@gmail.com>
2024-03-21 23:48:54 -0500

    add header to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,10 @@
-import { ClerkProvider } from '@clerk/nextjs'
+import {
+	ClerkProvider,
+	SignInButton,
+	SignedIn,
+	SignedOut,
+	UserButton
+} from '@clerk/nextjs'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './styling/globals.css'
@@ -18,7 +24,17 @@ export default function RootLayout({
 	return (
 		<ClerkProvider>
 			<html lang='en'>
-				<body className={inter.className}>{children}</body>
+				<body className={inter.className}>
+					<header>
+						<SignedOut>
+							<SignInButton />
+						</SignedOut>
+						<SignedIn>
+							<UserButton />
+						</SignedIn>
+					</header>
+					{children}
+				</body>
 			</html>
 		</ClerkProvider>
 	)
