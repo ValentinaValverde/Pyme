742e4162ca64b12560d912742f2c2067cfe70f62
Valentina Valverde <vvgreenville@gmail.com>
2024-03-22 12:48:33 -0400

    - Renamed StoreAddressForm to CreateStoreAddressForm - Also renamed the import / export for that component (same as above) - Moved CreateStoreAddressForm to create-forms folder in components

---
+++ components/create-forms/CreateStoreAddressForm.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import React from 'react';
+import { createStoreAddress } from '@/utils/actions/storeActions';
+
+const CreateStoreAddressForm = ({ myStore }: any) => {
+  return (
+    <form action={createStoreAddress}>
+      <div className="join w-full">
+        <input
+          className="input input-bordered join-item w-full"
+          placeholder="Street Address"
+          type="text"
+          name="streetAddress"
+          required
+        />
+        <input
+          className="input input-bordered join-item w-full"
+          placeholder="City"
+          type="text"
+          name="city"
+          required
+        />
+        <input
+          className="input input-bordered join-item w-full"
+          placeholder="State"
+          type="text"
+          name="state"
+          required
+        />
+        <input
+          className="input input-bordered join-item w-full"
+          placeholder="Zip Code"
+          type="text"
+          name="zipcode"
+          required
+        />
+        <input type="hidden" name="myStore" value={myStore} />
+        <button type="submit" className="btn join-item  btn-primary">
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default CreateStoreAddressForm;
