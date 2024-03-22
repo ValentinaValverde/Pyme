58c76bc73d9c6bc6c9dd13364ff7d6d1e2eb65b3
rugby8724 <jeremiah.d.wise@gmail.com>
2024-03-22 10:43:52 -0500

    Change name to slug in edit product link
    change Image to img on StoreStory component

--- app/mystore/[slug]/products/[product]/page.tsx
+++ app/mystore/[slug]/products/[product]/page.tsx
@@ -5,25 +5,25 @@ import Sidebar from '@/components/Sidebar'
 import { Box } from '@mui/material'
 
 const editProductPage = async ({ params }: { params: any }) => {
-  const productDoc = await getProduct(params.product)
-  const product = productDoc.toObject()
-  const myStore = params.slug
+	const productDoc = await getProduct(params.product)
+	const product = productDoc.toObject()
+	const myStore = params.slug
 
-  // Remove non-serializable properties
-  delete product._id
-  delete product.__v
-  delete product.productStoreId
+	// Remove non-serializable properties
+	delete product._id
+	delete product.__v
+	delete product.productStoreId
 
-  return (
-    <>
-      <Box sx={{ display: 'flex' }}>
-        <Sidebar storeSlug={myStore} />
-        <Box component={'main'} sx={{ flexGrow: 1, p: 3 }}>
-          <EditProductForm product={product} myStore={myStore} />
-        </Box>
-      </Box>
-    </>
-  )
+	return (
+		<>
+			<Box sx={{ display: 'flex' }}>
+				<Sidebar storeSlug={myStore} />
+				<Box component={'main'} sx={{ flexGrow: 1, p: 3 }}>
+					<EditProductForm product={product} myStore={myStore} />
+				</Box>
+			</Box>
+		</>
+	)
 }
 
 export default editProductPage
--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -15,62 +15,64 @@ import EditIcon from '@mui/icons-material/Edit'
 import Link from 'next/link'
 
 const ProductList = async ({ myStore }: any) => {
-  const products = await getStoreProducts(myStore)
+	const products = await getStoreProducts(myStore)
 
-  if (products.length === 0) {
-    return (
-      <h2 className="mt-8 font-medium text-lg">
-        Currently you do not have any active products
-      </h2>
-    )
-  }
+	if (products.length === 0) {
+		return (
+			<h2 className='mt-8 font-medium text-lg'>
+				Currently you do not have any active products
+			</h2>
+		)
+	}
 
-  return (
-    <TableContainer component={Paper}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell style={{ fontWeight: 'bold' }}>Name</TableCell>
-            <TableCell style={{ fontWeight: 'bold' }}>Details</TableCell>
-            <TableCell style={{ fontWeight: 'bold' }}>Price</TableCell>
-            <TableCell style={{ fontWeight: 'bold' }}>Inventory</TableCell>
-            <TableCell style={{ fontWeight: 'bold' }}>Image</TableCell>
-            <TableCell style={{ fontWeight: 'bold' }}>Actions</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {products.map((product) => (
-            <TableRow key={product.id}>
-              <TableCell>{product.productName}</TableCell>
-              <TableCell>{product.productDetails}</TableCell>
-              <TableCell>{product.price}</TableCell>
-              <TableCell>{product.inInv}</TableCell>
-              <TableCell>
-                {product.productImage && (
-                  <Image
-                    loading="lazy"
-                    src={product.productImage}
-                    alt={product.productName}
-                    className="max-w-xs max-h-24"
-                    width={100}
-                    height={100}
-                  />
-                )}
-              </TableCell>
-              <TableCell>
-			  <Link href={`/mystore/${myStore}/products/${product.productName}`}>
-				<Tooltip title="Edit Product">
-                <Button variant="contained">
-                  <EditIcon color="action"></EditIcon>
-                </Button>
-				</Tooltip>
-				</Link>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  )
+	return (
+		<TableContainer component={Paper}>
+			<Table aria-label='simple table'>
+				<TableHead>
+					<TableRow>
+						<TableCell style={{ fontWeight: 'bold' }}>Name</TableCell>
+						<TableCell style={{ fontWeight: 'bold' }}>Details</TableCell>
+						<TableCell style={{ fontWeight: 'bold' }}>Price</TableCell>
+						<TableCell style={{ fontWeight: 'bold' }}>Inventory</TableCell>
+						<TableCell style={{ fontWeight: 'bold' }}>Image</TableCell>
+						<TableCell style={{ fontWeight: 'bold' }}>Actions</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{products.map((product) => (
+						<TableRow key={product.id}>
+							<TableCell>{product.productName}</TableCell>
+							<TableCell>{product.productDetails}</TableCell>
+							<TableCell>{product.price}</TableCell>
+							<TableCell>{product.inInv}</TableCell>
+							<TableCell>
+								{product.productImage && (
+									<Image
+										loading='lazy'
+										src={product.productImage}
+										alt={product.productName}
+										className='max-w-xs max-h-24'
+										width={100}
+										height={100}
+									/>
+								)}
+							</TableCell>
+							<TableCell>
+								<Link
+									href={`/mystore/${myStore}/products/${product.productSlug}`}
+								>
+									<Tooltip title='Edit Product'>
+										<Button variant='contained'>
+											<EditIcon color='action'></EditIcon>
+										</Button>
+									</Tooltip>
+								</Link>
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	)
 }
 export default ProductList
