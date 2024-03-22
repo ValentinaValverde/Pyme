5fec911563c1e9683eb7d2d72995216b29683827
Valentina Valverde <vvgreenville@gmail.com>
2024-03-22 13:13:13 -0400

    - Deleted customerfacing.css file - Deleted storeadminfacing.css file - Deleted a folder in components ("admin" or "store-owner" or something) - Deleting that folder above deleted these two files: 	- OwnerInfoCards component 	- OwnerSignUp component

--- app/mystore/[slug]/page.tsx
+++ app/mystore/[slug]/page.tsx
@@ -1,20 +1,18 @@
-"use client";
-import React from "react";
-import Sidebar from "@/components/Sidebar";
-import { Box } from "@mui/material";
-import { useParams } from "next/navigation";
-import OwnerInfoCards from "@/components/store-owner-site/OwnerInfoCards";
+'use client';
+import React from 'react';
+import Sidebar from '@/components/Sidebar';
+import { Box } from '@mui/material';
+import { useParams } from 'next/navigation';
 
 const MyStoreHome = () => {
   const params = useParams();
-  const slug = params.slug?.toString() || "";
+  const slug = params.slug?.toString() || '';
   return (
     <>
-      <Box sx={{ display: "flex" }}>
+      <Box sx={{ display: 'flex' }}>
         <Sidebar storeSlug={slug} />
-        <Box component={"main"} sx={{ flexGrow: 1, p: 3 }}>
+        <Box component={'main'} sx={{ flexGrow: 1, p: 3 }}>
           <div>Store Home</div>
-          <OwnerInfoCards />
         </Box>
       </Box>
     </>
