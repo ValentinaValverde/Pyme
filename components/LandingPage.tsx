ed7d4f716da37932096016865fa476e8f603edab
Valentina Valverde <vvgreenville@gmail.com>
2024-03-22 14:13:50 -0400

    Created Navbar component, imported it into LandingPage component so it only renders on that page. Might need to change in the future.

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,19 +1,21 @@
-import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+import Navbar from './Navbar';
 
 export default function LangingPage() {
-	return (
-		<>
-			<div className='landing_page_div'>
-				<p>welcome to</p>
-				<h1>Pyme</h1>
-				<p>the marketplace for small businesses.</p>
-				<SignedOut>
-					<SignInButton />
-				</SignedOut>
-				<SignedIn>
-					<UserButton />
-				</SignedIn>
-			</div>
-		</>
-	)
+  return (
+    <>
+      <Navbar />
+      <div className="landing_page_div">
+        <p>welcome to</p>
+        <h1>Pyme</h1>
+        <p>the marketplace for small businesses.</p>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </>
+  );
 }
