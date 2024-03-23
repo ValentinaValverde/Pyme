05dae9058a5caf0b9be1c4dc354c43a6ecbe35ed
jlotten <jenna.otten@gmail.com>
2024-03-23 13:44:24 -0700

    Added store story page that connects to home on sidebar

---
+++ app/mystore/[slug]/story/edit/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import StoreStoryForm from '@/components/create-forms/StoreStoryForm'
+import { getStoreStory } from '@/utils/actions/storeActions'
+import Sidebar from '@/components/Sidebar'
+import { Box, Button } from '@mui/material'
+
+const editStoryPage = async ({ params }) => {
+  const slug = params.slug?.toString() || ''
+  const story = await getStoreStory(params.slug)
+  return (
+    <>
+      <Box sx={{ display: 'flex' }}>
+        <Sidebar storeSlug={slug} />
+        <Box component={'main'} sx={{ flexGrow: 1, p: 3 }}>
+          <StoreStoryForm story={story} myStore={slug} />
+        </Box>
+      </Box>
+    </>
+  )
+}
+
+export default editStoryPage
