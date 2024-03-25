12bef71355889e982a37a239c12de248f2846eb4
jlotten <jenna.otten@gmail.com>
2024-03-25 11:39:49 -0700

    Changed Owner Name formfield label to Owner First & Last Name

--- components/StoreSignUpForm.tsx
+++ components/StoreSignUpForm.tsx
@@ -57,7 +57,7 @@ const StoreSignUpForm = () => {
 
 								<div className='sm:col-span-4'>
 									<label className='block text-sm font-medium leading-6 text-gray-900'>
-										Owner Name
+										Owner First & Last Name
 									</label>
 									<input
 										className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
