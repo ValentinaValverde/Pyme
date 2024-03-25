f2a28858a69d6893ebbd148ce8779377d552604b
jlotten <jenna.otten@gmail.com>
2024-03-25 12:36:28 -0700

    Moved product description to be above text area

--- components/create-forms/CreateProductForm.tsx
+++ components/create-forms/CreateProductForm.tsx
@@ -32,6 +32,9 @@ const CreateProductForm = ({ myStore }: any) => {
 									<label className='block text-sm font-medium leading-6 text-gray-900'>
 										Product Details
 									</label>
+									<p className='mt-3 text-sm leading-6 text-gray-600'>
+										Describe your product
+									</p>
 									<textarea
 										className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
 										rows={3}
@@ -39,9 +42,6 @@ const CreateProductForm = ({ myStore }: any) => {
 										name='productDetails'
 										required
 									/>
-									<p className='mt-3 text-sm leading-6 text-gray-600'>
-										Describe your product
-									</p>
 								</div>
 
 								{/* <div className="col-span-full">
