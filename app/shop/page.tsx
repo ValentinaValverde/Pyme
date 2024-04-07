d2e7a8ef5cce95bbdeec7dc9ab0bfd3716f91b5a
jlotten <jenna.otten@gmail.com>
2024-04-07 11:17:14 -0700

    Added an empty page for shop homepage

---
+++ app/shop/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        {/* TODO fill in the homepage with featured stores and list of stores */}
+      </div>
+    </>
+  )
+}
