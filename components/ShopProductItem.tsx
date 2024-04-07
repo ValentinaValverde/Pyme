f44612002a87c77b3704ccb4c222bb8b99c1d08f
rugby8724 <jeremiah.d.wise@gmail.com>
2024-04-07 15:29:25 -0500

    Add Add to cart button to store home page
    Add onclick handler for add to cart

--- components/ShopProductItem.tsx
+++ components/ShopProductItem.tsx
@@ -1,35 +1,43 @@
-import React from 'react'
+'use client'
+
 import { Product } from '@/lib/models/ProductModel'
 import Link from 'next/link'
+import { createCartItem } from '@/utils/actions/cartItemActions'
 
 export default function ShopProductItem({
-  product,
-  storeSlug,
+	product,
+	storeSlug
 }: {
-  product: Product
-  storeSlug: string
+	product: Product
+	storeSlug: string
 }) {
-  return (
-    <div className="card bg-base-300 shadow-xl mb-4">
-      <figure>
-        <Link href={`${storeSlug}/product/${product.productSlug}`}>
-          <img
-            src={product.productImage}
-            alt={product.productName}
-            width={300}
-            height={300}
-            className="object-cover h-64 w-full"
-          />
-        </Link>
-      </figure>
-      <div className="card-body">
-        <Link href={`${storeSlug}/product/${product.productSlug}`}>
-          <h2 className="card-title font-normal">{product.productName}</h2>
-        </Link>
-        <div className="card-actions flex justify-between items-center">
-          <span className="text-2xl">${product.price}</span>{' '}
-        </div>
-      </div>
-    </div>
-  )
+	const addToCartHandler = async () => {
+		await createCartItem(product.productSlug, 1)
+	}
+	return (
+		<div className='card bg-base-300 shadow-xl mb-4'>
+			<figure>
+				<Link href={`${storeSlug}/product/${product.productSlug}`}>
+					<img
+						src={product.productImage}
+						alt={product.productName}
+						width={300}
+						height={300}
+						className='object-cover h-64 w-full'
+					/>
+				</Link>
+			</figure>
+			<div className='card-body'>
+				<Link href={`${storeSlug}/product/${product.productSlug}`}>
+					<h2 className='card-title font-normal'>{product.productName}</h2>
+				</Link>
+				<div className='card-actions flex justify-between items-center'>
+					<span className='text-2xl'>${product.price}</span>{' '}
+				</div>
+				<button onClick={addToCartHandler} className='btn btn-primary w-full'>
+					Add to Cart
+				</button>
+			</div>
+		</div>
+	)
 }
