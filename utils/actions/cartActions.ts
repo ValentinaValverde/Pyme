f4c5ddb364a97a0ac50058d8d71130f92ce7ccde
rugby8724 <jeremiah.d.wise@gmail.com>
2024-04-12 10:23:38 -0500

    Add stripe starter backend code

--- utils/actions/cartActions.ts
+++ utils/actions/cartActions.ts
@@ -2,8 +2,9 @@
 
 import dbConnect from '@/lib/dbConnect'
 import CartModel from '@/lib/models/CartModel'
+import CartItemModel from '@/lib/models/CartItemModel'
 import { auth } from '@clerk/nextjs/server'
-import { create } from 'zustand'
+import Stripe from 'stripe'
 
 export const createCart = async () => {
 	await dbConnect()
@@ -25,4 +26,50 @@ export const createCart = async () => {
 	} catch (error) {
 		console.error(error)
 	}
+}
+
+export const checkOut = async () => {
+	await dbConnect()
+
+	const { userId } = auth()
+
+	const cart = await CartModel.findOne({
+		$and: [{ userId }, { active: true }]
+	})
+
+	if (!cart) {
+		return
+	}
+
+	const cartItems = await CartItemModel.find({ cartId: cart.id })
+
+	const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+		apiVersion: '2024-04-10'
+	})
+
+	if (!stripe) {
+		throw new Error('Failed to initialize Stripe')
+	}
+
+	const session = await stripe.checkout.sessions.create({
+		payment_method_types: ['card'],
+		line_items: [
+			{
+				price_data: {
+					currency: 'usd',
+					product_data: {
+						name: 'T-shirt'
+					},
+					unit_amount: 2000
+				},
+				quantity: 1
+			}
+		],
+		mode: 'payment',
+		success_url: 'https://example.com/success',
+		cancel_url: 'https://example.com/cancel',
+		automatic_tax: { enabled: true }
+	})
+
+	return session
 }
