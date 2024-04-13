5faec7c4d9e8bd10c51fb77d92fc36a5342744b3
rugby8724 <jeremiah.d.wise@gmail.com>
2024-04-13 17:44:54 -0500

    Create Order and Order Item model
    Create get and create order actions

--- lib/models/OrderModel.ts
+++ lib/models/OrderModel.ts
@@ -1,9 +1,41 @@
-export type OrderItem = {
-    name: string;
-    slug: string;
-    qty: number;
-    image: string;
-    price: number;
-    color: string;
-    size: string;
-};
+// export type OrderItem = {
+//     name: string;
+//     slug: string;
+//     qty: number;
+//     image: string;
+//     price: number;
+//     color: string;
+//     size: string;
+// };
+
+import mongoose from 'mongoose'
+
+const OrderSchema = new mongoose.Schema(
+	{
+		cart_id: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: [true],
+			ref: 'Cart'
+		},
+		user_id: {
+			type: String,
+			required: [true]
+		},
+		store_id: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: [true],
+			ref: 'Store'
+		},
+		total_price: {
+			type: Number,
+			required: [true]
+		}
+	},
+	{
+		timestamps: true
+	}
+)
+
+const OrderModel = mongoose.models.Order || mongoose.model('Order', OrderSchema)
+
+export default OrderModel
