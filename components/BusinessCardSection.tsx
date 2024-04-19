6f10e84d3c5fe84159c947eb45a3abed2fee0071
Valentina Valverde <vvgreenville@gmail.com>
2024-04-19 19:08:17 -0400

    Functionality is there! Users can go to the main site and see the store cards, and when clicked on it sends them to that stores page.

--- components/BusinessCardSection.tsx
+++ components/BusinessCardSection.tsx
@@ -1,23 +1,31 @@
-import BusinessCard from './BusinessCard';
+import Link from 'next/link';
 
-export default function BusinessSection() {
+export default function BusinessSection({ stores }: any) {
+  console.log('STORES:', stores[0]);
+
   return (
     <>
       <div>
         <h1 className="center">shop by business</h1>
         <div className="businesscard_container">
-          <BusinessCard />
-          <BusinessCard />
-          <BusinessCard />
-          <BusinessCard />
-          <BusinessCard />
-          <BusinessCard />
-          <BusinessCard />
-          <BusinessCard />
-          <BusinessCard />
-          <BusinessCard />
-          <BusinessCard />
-          <BusinessCard />
+          {stores.map((store: any) => (
+            <Link href={`/shop/${store.slug}`} key={store.storeName}>
+              <div className="card w-96 bg-base-100 shadow-xl m-5">
+                <figure>
+                  <img
+                    src={store.storeImg}
+                    alt="Small Business Image"
+                    width="400"
+                    height="100"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">{store.storename}</h2>
+                  <p>By {store.ownername}</p>
+                </div>
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </>
