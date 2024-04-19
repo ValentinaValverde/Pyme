53471ac6543df966b4856bf5b4d455bfd84725b9
Valentina Valverde <vvgreenville@gmail.com>
2024-04-19 13:04:07 -0400

    Attempting to get featured stores

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Navbar from '@/components/Navbar';
 import Hero from '../components/Hero';
 import BusinessSection from '@/components/BusinessCardSection';
+import Featured from '@/components/Featured';
+
+import { getFeaturedStores } from '@/utils/actions/shopActions';
 
 export default function Home() {
   return (
     <main className="">
       <Navbar />
       <Hero />
+      <Featured featuredStores={{ getFeaturedStores }} />
       <BusinessSection />
     </main>
   );
--- utils/actions/shopActions.ts
+++ utils/actions/shopActions.ts
@@ -1,39 +1,74 @@
-'use server'
+'use server';
 
-import dbConnect from '@/lib/dbConnect'
-import StoreModel from '@/lib/models/StoreModel'
-import { Product, ProductModel } from '@/lib/models/ProductModel'
-import { StoreStory } from '@/lib/models/StoreStoryModel'
-import { StoreStoryModel } from '@/lib/models/StoreStoryModel'
+import dbConnect from '@/lib/dbConnect';
+import StoreModel from '@/lib/models/StoreModel';
+// import { StoreSchema } from '@/lib/models/StoreModel';
+import { Product, ProductModel } from '@/lib/models/ProductModel';
+import { StoreStory } from '@/lib/models/StoreStoryModel';
+import { StoreStoryModel } from '@/lib/models/StoreStoryModel';
 
 export const getStoreProducts = async (slug: string): Promise<Product[]> => {
-  await dbConnect()
+  await dbConnect();
 
-  const store = await StoreModel.findOne({ slug: slug })
+  const store = await StoreModel.findOne({ slug: slug });
 
-  const storeId = store.id
+  const storeId = store.id;
 
   const storeProducts = await ProductModel.find({
     productStoreId: storeId,
-  })
+  });
 
-  return storeProducts
-}
+  return storeProducts;
+};
 
 export const getStoreStory = async (
   storeSlug: string
 ): Promise<StoreStory | null> => {
-  await dbConnect()
-  const store = await StoreModel.findOne({ slug: storeSlug })
+  await dbConnect();
+  const store = await StoreModel.findOne({ slug: storeSlug });
   if (!store) {
-    return null
+    return null;
   }
-  const storeId = store.id
+  const storeId = store.id;
 
-  const story = await StoreStoryModel.findOne({ storeId: storeId })
+  const story = await StoreStoryModel.findOne({ storeId: storeId });
   if (!story) {
-    return null
+    return null;
   }
 
-  return story
-}
+  return story;
+};
+
+// getStore function goes here
+
+// pull / "get" stores from the db
+// randomize stores
+// display top 4 stores
+
+export const getStores = async () => {
+  await dbConnect();
+
+  const stores = await StoreModel.find();
+  return stores;
+};
+
+export const getFeaturedStores = async () => {
+  await dbConnect();
+
+  const allStores = await getStores();
+  const stores = allStores.map(async (store) => {
+    const story = await getStoreStory(store.slug);
+
+    return {
+      storeName: store.storeName,
+      storeImg: story?.storeImage,
+      storeStory: story?.storeDetails,
+    };
+  });
+
+  console.log(stores);
+
+  const shuffleStores = stores.sort((a, b) => 0.5 - Math.random());
+  console.log('SHUFFLED STORES', shuffleStores.slice(0, 3));
+  return shuffleStores.slice(0, 3);
+};
