50057aa0f4266a348de73b3d01b47812c4e51d9f
rugby8724 <jeremiah.d.wise@gmail.com>
2024-04-20 14:38:05 -0500

    Add the ability to get customer orders
    Created page.js and component

--- app/shop/orders/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { createOrder } from '@/utils/actions/orderActions'
-
-const customerOrderPage = async () => {
-	await createOrder()
-	return <div>Processing Order...</div>
-}
-
-export default customerOrderPage
