8fd718764bd24073e0d2e7a47f12bc179b57f051
jlotten <jenna.otten@gmail.com>
2024-04-26 15:30:22 -0700

    Changed text, syle and location of message

--- app/shop/process_order/page.tsx
+++ app/shop/process_order/page.tsx
@@ -3,19 +3,45 @@ import React, { useState, useEffect } from 'react'
 import { createOrder } from '@/utils/actions/orderActions'
 
 const ProcessOrderPage = () => {
-	const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false)
 
-	useEffect(() => {
-		const processOrder = async () => {
-			setLoading(true)
-			await createOrder()
-			setLoading(false)
-		}
+  useEffect(() => {
+    const processOrder = async () => {
+      setLoading(true)
+      await createOrder()
+      setLoading(false)
+    }
 
-		processOrder()
-	}, [])
+    processOrder()
+  }, [])
 
-	return loading ? <div>Processing Order...</div> : <div>Order Processed</div>
+  return loading ? (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '70vh',
+        fontSize: '24px',
+        fontWeight: 'bold',
+      }}
+    >
+      Processing Order...
+    </div>
+  ) : (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '70vh',
+        fontSize: '24px',
+        fontWeight: 'bold',
+      }}
+    >
+      Your order has been processed.
+    </div>
+  )
 }
 
 export default ProcessOrderPage
