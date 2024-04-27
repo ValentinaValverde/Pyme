0cd66191614fd64061c26e676c65965a97de55b2
rugby8724 <jeremiah.d.wise@gmail.com>
2024-04-27 14:53:47 -0500

    Create dropdown to change qty in Cart.tsx

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -16,6 +16,7 @@ import { CartItemDetail, deleteCartItem } from '@/utils/actions/cartItemActions'
 import DeleteIcon from '@mui/icons-material/Delete'
 import { createCartItem } from '@/utils/actions/cartItemActions'
 import AddIcon from '@mui/icons-material/Add'
+import QtyDropDown from './QtyDropDown'
 
 import { Card, CardContent, Typography } from '@mui/material'
 
@@ -23,87 +24,6 @@ import { Card, CardContent, Typography } from '@mui/material'
 const stripePromise = loadStripe(
 	process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
 )
-/*
-// export function CartOld({ cartItems }: any) {
-// 	React.useEffect(() => {
-// 		const query = new URLSearchParams(window.location.search)
-// 		if (query.get('success')) {
-// 			alert('Order placed! You will receive an email confirmation.')
-// 		}
-// 		if (query.get('canceled')) {
-// 			alert(
-// 				'Order canceled -- continue to shop around and checkout when you’re ready.'
-// 			)
-// 		}
-// 	}, [])
-
-// 	const handleCheckout = async (event: any) => {
-// 		event.preventDefault()
-// 		const stripe = await stripePromise
-// 		const { id }: any = await checkOut()
-// 		const result = await stripe!.redirectToCheckout({
-// 			sessionId: id
-// 		})
-// 		if (result.error) {
-// 			alert(result.error.message)
-// 		}
-// 	}
-
-// 	if (!cartItems || cartItems.length === 0) {
-// 		return <div>There are currently no items in your cart</div>
-// 	}
-
-// 	return (
-// 		<>
-// 			<div>
-// 				{cartItems.map((item: any) => (
-// 					<div key={item.id}>
-// 						<h1>{item.productName}</h1>
-// 						<p>Price: {item.priceAtTime}</p>
-// 						<p>Quantity: {item.quantity}</p>
-// 					</div>
-// 				))}
-// 			</div>
-// 			<form>
-// 				<section>
-// 					<button onClick={handleCheckout} type='button' role='link'>
-// 						Checkout
-// 					</button>
-// 				</section>
-// 				<style jsx>
-// 					{`
-// 						section {
-// 							background: #ffffff;
-// 							display: flex;
-// 							flex-direction: column;
-// 							width: 400px;
-// 							height: 112px;
-// 							border-radius: 6px;
-// 							justify-content: space-between;
-// 						}
-// 						button {
-// 							height: 36px;
-// 							background: #556cd6;
-// 							border-radius: 4px;
-// 							color: white;
-// 							border: 0;
-// 							font-weight: 600;
-// 							cursor: pointer;
-// 							transition: all 0.2s ease;
-// 							box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
-// 						}
-// 						button:hover {
-// 							opacity: 0.8;
-// 						}
-// 					`}
-// 				</style>
-// 			</form>
-// 		</>
-// 	)
-// }
-*/
-   
-
 
 export function Cart({
 	initialCartItems
@@ -185,15 +105,10 @@ return (
 								)}{' '}
 							</TableCell>
 							<TableCell>
-								{product.quantity}
+                <QtyDropDown slug={product.productSlug} quantity={product.quantity} />
 								<Tooltip title="Delete">
 								<Button onClick={() => _deleteCartItem(product.productSlug)}>
 									<DeleteIcon />
-								</Button>
-								</Tooltip>
-								<Tooltip title="Add Item">
-								<Button onClick={() => _createCartItem(product.productSlug, 1, product.quantity)}>
-									<AddIcon />
 								</Button>
 								</Tooltip>
 							</TableCell>
@@ -229,4 +144,3 @@ return (
 	</>
 )
 }
-
---
+++ components/QtyDropDown.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select, { SelectChangeEvent } from '@mui/material/Select'
+import { editCartItemQuantity } from '@/utils/actions/cartItemActions'
+
+const QtyDropDown = ({ slug, quantity }: { slug: string; quantity: any }) => {
+	const handleChange = async (event: SelectChangeEvent) => {
+		event.preventDefault()
+		let newQuantity = Number(event.target.value)
+		await editCartItemQuantity(slug, newQuantity)
+    location.reload()
+	}
+	quantity.toString()
+	return (
+		<Box sx={{ minWidth: 120 }}>
+			<FormControl fullWidth>
+				<Select
+					labelId='product-quantity-label'
+					id='product-quantity'
+					defaultValue={quantity}
+					onChange={handleChange}
+				>
+					<MenuItem value={0}>0</MenuItem>
+					<MenuItem value={1}>1</MenuItem>
+					<MenuItem value={2}>2</MenuItem>
+					<MenuItem value={3}>3</MenuItem>
+					<MenuItem value={4}>4</MenuItem>
+					<MenuItem value={5}>5</MenuItem>
+					<MenuItem value={6}>6</MenuItem>
+					<MenuItem value={7}>7</MenuItem>
+					<MenuItem value={8}>8</MenuItem>
+					<MenuItem value={9}>9</MenuItem>
+					<MenuItem value={10}>10</MenuItem>
+				</Select>
+			</FormControl>
+		</Box>
+	)
+}
+
+export default QtyDropDown
