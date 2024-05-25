e9b36af93a7fd9f372a31f2968af873d610e6aae
Valentina Valverde <vvgreenville@gmail.com>
2024-05-25 17:20:03 -0400

    Saving changes

---
+++ components/SearchBar.tsx
@@ -0,0 +1,12 @@
+import Fuse from 'fuse.js';
+// import Fuse from 'https://deno.land/x/fuse@v7.0.0/dist/fuse.min.mjs'
+
+export default function SearchBar() {
+  //   const Fuse = require('fuse.js');
+
+  return (
+    <>
+      <p>search bar</p>
+    </>
+  );
+}
